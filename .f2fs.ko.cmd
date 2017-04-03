6ea3f8382a1630737c064996146be74533a7e3da
fishpadak <dongyun.lee@csl.skku.edu>
2017-04-04 05:09:40 +0900

    .gitignore is now working

--- .f2fs.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/sally/f2fs/f2fs.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/sally/f2fs/f2fs.ko /home/sally/f2fs/f2fs.o /home/sally/f2fs/f2fs.mod.o ;  true
